6d678489b4ca36609b145c3e83194d2cb31adfef
Nahuel Cruz <ncruz@fi.uba.ar>
2024-11-14 18:18:01 -0300

    Cambios en Juego, Logica y en Diagramas

--- src/main/java/org/example/Juego.java
+++ src/main/java/org/example/Juego.java
@@ -1,26 +1,32 @@
 package org.example;
 
+import java.util.ArrayList;
+
 public class Juego {
     private Jugador jugador;
-    private Ronda ronda;
-    private int cantidadRondas;
+    private ArrayList<Ronda> rondas = new ArrayList<Ronda>();
+    private int numeroRonda = 0;
+    private int cantidadRondas = 8;
     private boolean juegoGanado = false;
 
+    public Juego(Puntaje[] limitesDePuntosPorRonda, Puntaje[][] limitesDePuntosPorTurno) {
+        for(int i = 0; i < cantidadRondas; i++) {
+            rondas.add(new Ronda(limitesDePuntosPorRonda[i], limitesDePuntosPorTurno[i]));
+        }
+    }
+
     public void jugar(){
-        /*
-        while(ronda.puntajeSuperoElLimite(jugador.jugar()) && cantidadRondas > 0){
 
-
-
-            cantidadRondas--;
+        while(rondas.get(numeroRonda).esGanada(jugador) && numeroRonda <= cantidadRondas){
+            numeroRonda++;
         }
 
-        if (cantidadRondas == 0) {
+        if (numeroRonda == 8) {
             juegoGanado = true;
         }
         juegoGanado = false;
-        */
     }
+
     public void PartidaPerdida(){
 
     }
--- src/main/java/org/example/Ronda.java
+++ src/main/java/org/example/Ronda.java
@@ -2,11 +2,24 @@ package org.example;
 
 public class Ronda {
     private Puntaje limiteDePuntos;
+    private Puntaje[] limitesDePuntoPorTurno;
+    private int cantidadDeTurnos = 5;
     private int turno;
 
-    public Ronda(Puntaje limiteDePuntos) {
+    public Ronda(Puntaje limiteDePuntos, Puntaje[] limitesDePuntosPorTurno) {
         this.limiteDePuntos = limiteDePuntos;
+        this.limitesDePuntoPorTurno = limitesDePuntosPorTurno;
     }
+
+    public boolean esGanada(Jugador jugador){
+
+
+
+        ;
+    }
+
+
+
     public void pasarTurno(Puntaje puntajeTotal){
         this.turno -= 1;
         if(limiteDePuntos.esMenorA(puntajeTotal));

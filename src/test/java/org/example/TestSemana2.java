4c450213570a83cd6424a7cbe9715b14deba488e
Jonathan Huergo <jh2000@hotmail.com.ar>
2024-11-24 19:28:43 -0300

    Usar mockito en test 4 de la segunda entrega

--- src/test/java/org/example/TestSemana2.java
+++ src/test/java/org/example/TestSemana2.java
@@ -2,11 +2,14 @@ package org.example;
 
 import org.example.Comodin.*;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 
 import java.util.ArrayList;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
 
 
 public class TestSemana2 {
@@ -18,7 +21,7 @@ public class TestSemana2 {
         Carta carta = new Carta("diamantes", 5);
         manoPoker.agregarCarta(carta);
         Comodin comodin = new ComodinBase(0,8,1, new ActivacionSiempre());
-        ArrayList<Comodin> comodines = new ArrayList<Comodin>();
+        ArrayList<Comodin> comodines = new ArrayList<>();
         comodines.add(comodin);
         Jugador jugador = new Jugador();
         jugador.setManoPoker(manoPoker);
@@ -41,7 +44,7 @@ public class TestSemana2 {
         manoPoker.agregarCarta(new Carta("corazon", 5));
 
         Comodin comodin = new ComodinBase(0,0,3,  new ActivacionTipoDeMano("Escalera"));
-        ArrayList<Comodin> comodines = new ArrayList<Comodin>();
+        ArrayList<Comodin> comodines = new ArrayList<>();
         comodines.add(comodin);
 
         Jugador jugador = new Jugador();
@@ -61,7 +64,7 @@ public class TestSemana2 {
         Carta carta = new Carta("diamantes", 5);
         manoPoker.agregarCarta(carta);
         Comodin comodin = new ComodinPorDescarte(0,0,1, new ActivacionDescartes());
-        ArrayList<Comodin> comodines = new ArrayList<Comodin>();
+        ArrayList<Comodin> comodines = new ArrayList<>();
         comodines.add(comodin);
         Jugador jugador = new Jugador();
         jugador.setManoPoker(manoPoker);
@@ -73,22 +76,26 @@ public class TestSemana2 {
 
     @Test
     public void test04ComodinConProbabilidad1Sobre1000TieneEfecto() {
-        // Usar mokito !!!
         int puntajeEsperado = 40; // Carta Alta: 5 * 1 -> 10 * 1 por ser un 5 -> 40 * 1 puntaje adicional
+
+        ActivacionProbabilidad activacionMock = Mockito.mock(ActivacionProbabilidad.class);
+        when(activacionMock.revisarCondicion(any(Jugador.class))).thenReturn(true);
 
         ManoPoker manoPoker = new ManoPoker();
         Carta carta = new Carta("diamantes", 5);
-        manoPoker.agregarCarta(carta);
-        Comodin comodin = new ComodinBase(30,0,1, new ActivacionProbabilidad(1000));
-        ArrayList<Comodin> comodines = new ArrayList<Comodin>();
-        comodines.add(comodin);
+
+        Comodin comodin = new ComodinBase(30, 0, 1, activacionMock);
+        ArrayList<Comodin> comodines = new ArrayList<>();
+
         Jugador jugador = new Jugador();
+        manoPoker.agregarCarta(carta);
+        comodines.add(comodin);
         jugador.setManoPoker(manoPoker);
         jugador.setComodines(comodines);
+
         jugador.evaluarMano();
 
-//        assertTrue(jugador.puntajeEsIgual(puntajeEsperado));
-
+        assertTrue(jugador.puntajeEsIgual(puntajeEsperado));
     }
 
     @Test

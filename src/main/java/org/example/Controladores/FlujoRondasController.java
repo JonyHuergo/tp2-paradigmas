9d53c4ec32ccd70b53ee3420fb31b2d3ec3ede61
FelipeVO <fvarelao@fi.uba.ar>
2024-12-10 16:23:36 -0300

    update numero de rondas

--- src/main/java/org/example/Controladores/FlujoRondasController.java
+++ src/main/java/org/example/Controladores/FlujoRondasController.java
@@ -33,50 +33,22 @@ public class FlujoRondasController {
             Juego juego = new Juego(mazo);
             Jugador jugador = juego.getJugador();
 
-            for(int i = 0; i <= rondas.size()-1; i++) {
-                Ronda ronda = rondas.get(0);
-                int descartes = ronda.getDescartes();
-                jugador.setCantidadDeDescartes(descartes);
+            int numeroRonda = 0;
+            Ronda ronda = rondas.get(numeroRonda);
+            int descartes = ronda.getDescartes();
+            jugador.setCantidadDeDescartes(descartes);
 
-                int manos= ronda.getCantidadDeManos();
-                jugador.setCantidadDeManos(manos);
+            int manos= ronda.getCantidadDeManos();
+            jugador.setCantidadDeManos(manos);
 
-                int numeroRonda = 0;
 
-                PantallaTiendaController pantallaTienda = new PantallaTiendaController(stage, mediaPlayer, mazo, rondas, numeroRonda, jugador);
-                pantallaTienda.iniciarPantallaTienda(ronda.obtenerTienda());
-            }
+
+            PantallaTiendaController pantallaTienda = new PantallaTiendaController(stage, mediaPlayer, mazo, rondas, numeroRonda, jugador);
+            pantallaTienda.iniciarPantallaTienda(ronda.obtenerTienda());
+
 
 
             PantallaJuegoController pantallaJuego= new PantallaJuegoController(stage, mediaPlayer);
-//            pantallaJuego.iniciarPantallaJuego(mazo, ronda, jugador);
 
-
-//            for (int i = ronda.getCantidadDeManos()-1; i<=0; i--){
-//                pantallaJuego.iniciarPantallaJuego(mazo, ronda, jugador);
-//            }
-
-
-//        for (int i = rondas.size() - 1; i >= 0; i--) {
-//            PantallaTiendaController pantallaTienda = new PantallaTiendaController(stage, mediaPlayer, mazo, ronda, jugador);
-//            pantallaTienda.iniciarPantallaTienda(ronda.obtenerTienda());
-//            Ronda currentRonda = rondas.get(i);
-//            for (int j = currentRonda.getCantidadDeManos() - 1; j >= 0; j--) {
-//                int descartes = ronda.getDescartes();
-//                jugador.setCantidadDeDescartes(descartes);
-//
-//
-//            }
-
-
-//        }
-
-//            for (int i = rondas.size() - 1; i >= 0; i--) {
-//
-////                Ronda currentRonda = rondas.get(i);
-////                ArrayList<Carta> cartasIniciales = jugador.getMazo().repartirCartas(5);
-////                int puntajeASuperar = currentRonda.getPuntajeASuperar();
-////                mostrarRonda(primaryStage, cartasIniciales, puntajeASuperar);
-//            }
         }
 }
